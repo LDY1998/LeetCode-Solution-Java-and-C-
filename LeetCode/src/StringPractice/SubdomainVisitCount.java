00e5df78185d8d9773d87aec7ca000306d08e307
Jerry <30492120+LDY1998@users.noreply.github.com>
2019-05-20 14:27:10 -0700

    Update SubdomainVisitCount

--- LeetCode/src/StringPractice/SubdomainVisitCount.java
+++ LeetCode/src/StringPractice/SubdomainVisitCount.java
@@ -2,7 +2,9 @@ package StringPractice;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SubdomainVisitCount {
 
@@ -10,23 +12,38 @@ public class SubdomainVisitCount {
 	
 	public static List<String> subdomainVisits(String[] cpdomains) {
         List<String> list=new ArrayList<>();
+        Map<String,Integer> map=new HashMap<>();
         for(String cpdomain:cpdomains){
-            list.add(cpdomain);
             String[] d=cpdomain.split(" ");
             
             String s1=d[1];
+            if(!map.containsKey(s1)) {
+            	map.put(s1, Integer.valueOf(d[0]));
+            }
+            else {
+            	map.put(s1, map.get(s1)+Integer.valueOf(d[0]));
+            }
             while(s1.contains(".")){
                  s1=s1.substring(s1.indexOf(".")+1);
-                list.add(d[0]+" "+s1);
+                 if(!map.containsKey(s1)) {
+                 	map.put(s1, Integer.valueOf(d[0]));
+                 }
+                 else {
+                 	map.put(s1, map.get(s1)+Integer.valueOf(d[0]));
+                 }
+                
             }
-            
-            
         }
+        
+        for(String key:map.keySet()) {
+        	list.add(map.get(key)+" "+key);
+        }
+        
         return list;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] s= {"9001 discuss.leetcode.com"};
+		String[] s= {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
 		List<String> list=subdomainVisits(s);
 		for(String s1:list) {
 			System.out.println(s1);
