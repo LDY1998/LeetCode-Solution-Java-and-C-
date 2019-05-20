6ff4f57e19552fa2374103c4c2761402630e01fc
Jerry <30492120+LDY1998@users.noreply.github.com>
2019-05-20 16:01:18 -0700

    Upload Reverse String problem

---
+++ LeetCode/src/StringPractice/ReverseString.java
@@ -0,0 +1,51 @@
+package StringPractice;
+
+/*
+ * Write a function that reverses a string. 
+ * The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, 
+you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable 
+ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ */
+
+
+
+
+
+public class ReverseString {
+
+	public static void reverse(char[] s) {
+		int left=0;int right=s.length-1;
+		while(left<right) {
+			char temp=s[left];
+			s[left]=s[right];
+			s[right]=temp;
+			left++;right--;
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String t1="hello";
+		char[] c=t1.toCharArray();
+		reverse(c);
+		for(char cc:c) {
+			System.out.print(cc);
+		}
+		
+	}
+
+}
